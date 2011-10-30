024cd08de2e7c5aa14d807441a34773319b1c6de
Daniel Palmer <danieruru@gmail.com>
2011-10-29 21:39:24 -0700

    Add phy<->virt mapping stuff I missed

--- arch/arm/mach-at91/include/mach/memory.h
+++ arch/arm/mach-at91/include/mach/memory.h
@@ -23,4 +23,39 @@
 
 #include <mach/hardware.h>
 
+#ifdef CONFIG_MACH_PICOSAM9G45
+
+#ifdef CONFIG_SPARSEMEM
+
+/*
+ * Sparsemem definitions for pico-SAM9G45
+ *
+ * The old patch for this had the high block mapped below the low block;
+ * Code in other parts (mm.c if I remember correctly) do not like this..
+ * The mapping below only works if the kernel is loaded into the lower 
+ * bank. Check Makefile.boot for how this is achieved.
+ *
+ * 128MB @ 0x20000000 -> PAGE_OFFSET
+ * 128MB @ 0x70000000 -> PAGE_OFFSET + 0x8000000
+ */
+
+#define BANK1 UL(0x20000000)
+#define BANK2 UL(0x70000000)
+#define BANKSIZE UL(0x8000000)
+#define PLAT_PHYS_OFFSET      BANK1
+#define MAX_PHYSMEM_BITS      32
+#define SECTION_SIZE_BITS     27 /*128 Mb */
+
+/* bank page offsets */
+#define PAGE_OFFSET_HIGHBANK  (PAGE_OFFSET + BANKSIZE)
+
+ #define __phys_to_virt(p)   \
+            (((p) & 0x07ffffff) + (((p) & 0x40000000) ? PAGE_OFFSET_HIGHBANK : PAGE_OFFSET))
+
+#define __virt_to_phys(v)   \
+            (((v) & 0x07ffffff) + (((v) & 0x08000000) ? BANK2 : BANK1 ))
+
+#endif        /* CONFIG_SPARSEMEM */
+#endif /* CONFIG_MACH_PICOSAM9G45 */
+
 #endif
